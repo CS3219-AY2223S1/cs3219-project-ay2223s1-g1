2575d51bc1e095994e8ec9dddf3f6ebe8a955f63
itssodium <noorulazlina@gmail.com>
2022-09-04 16:07:59 +0800

    create user model

--- matching-service/index.js
+++ matching-service/index.js
@@ -2,25 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
 import Sequelize from 'sequelize';
+import User from '../matching-service/models/user.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-
-// Setup DB Connection with sqlite3
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
-});
-
-try {
-    await sequelize.authenticate();
-    console.log('Connection to database sqlite3 has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database sqlite3:', error);
-}
 
 app.get('/', (req, res) => {
     res.send('Hello World from matching-service');
