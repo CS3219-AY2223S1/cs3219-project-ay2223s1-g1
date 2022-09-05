dd1d9d6aef8fec219c54c7146380eacb057f45a6
Lee Han Bin <65403285+habi39@users.noreply.github.com>
2022-09-05 17:51:33 +0800

    debugging of connection to local mongodb

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -6,7 +6,12 @@ import mongoose from 'mongoose';
 
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+if(mongoDB == undefined){
+  mongoose.connect("mongodb://localhost:27017/mydb", { useNewUrlParser: true , useUnifiedTopology: true});
+} else{
+  mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+}
+
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
