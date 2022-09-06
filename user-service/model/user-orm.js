685487246ae7bf69ba2ceb3a62ab766caa3deb67
Lee Han Bin <65403285+habi39@users.noreply.github.com>
2022-09-06 16:53:54 +0800

    Password hash

--- user-service/model/user-orm.js
+++ user-service/model/user-orm.js
@@ -1,9 +1,10 @@
 import { createUser } from './repository.js';
-
+import bcrypt from 'bcrypt';
 //need to separate orm functions from repository to decouple business logic from persistence
 export async function ormCreateUser(username, password) {
     try {
-        const newUser = await createUser({username, password});
+        const hash = bcrypt.hashSync(password, 10);
+        const newUser = await createUser({username:username, password:hash});
         newUser.save();
         return true;
     } catch (err) {
