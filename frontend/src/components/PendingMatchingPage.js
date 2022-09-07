70698a626bb01f637dbd56b2f1f59e5339c12fe6
Sagar Sureka <sagar150102@gmail.com>
2022-09-07 22:39:46 +0800

    Remove log and add comment

--- frontend/src/components/PendingMatchingPage.js
+++ frontend/src/components/PendingMatchingPage.js
@@ -7,7 +7,10 @@ import { useLocation } from "react-router-dom";
 function PendingMatchPage() {
     const location = useLocation()
     const { diff } = location.state
-    console.log(diff)
+
+    // Call to backend with difficulty level to match users
+    // Front end to display relevant message and wait for 30 seconds or a match
+    
     return (
         <Box display={"flex"} flexDirection={"column"} width={"70%"}>
             <Typography variant={"h3"} marginBottom={"2rem"}>Will match you to a user here!</Typography>
