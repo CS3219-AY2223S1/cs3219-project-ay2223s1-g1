99f147139366289246c5d77442e4fadfe2373ad8
Thomas Mertens <thomas.mertens7@outlook.com>
2022-09-09 18:15:14 +0800

    frontend signin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import SigninPage from './components/SigninPage';
 import {Box} from "@mui/material";
 
 function App() {
+
     return (
         <div className="App">
             <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
@@ -10,6 +12,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/signin" element={<SigninPage/>}/>
                     </Routes>
                 </Router>
             </Box>
---
+++ frontend/src/components/SigninPage.js
@@ -0,0 +1,96 @@
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    TextField,
+    Typography
+} from "@mui/material";
+import {useState} from "react";
+import axios from "axios";
+import {URL_USER_SVC} from "../configs";
+import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
+import {Link} from "react-router-dom";
+
+function SigninPage() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [dialogTitle, setDialogTitle] = useState("")
+    const [dialogMsg, setDialogMsg] = useState("")
+    const [isSigninSuccess, setIsSigninSuccess] = useState(false)
+
+    const handleSignin = async () => {
+        console.log("Woehoew")
+        setIsSigninSuccess(false)
+        const res = await axios.get(URL_USER_SVC, { username, password })
+            .catch((err) => {
+                setErrorDialog('Please try again later')
+            })
+        console.log(res)
+        if (res && res.status === STATUS_CODE_CREATED) {
+            setSuccessDialog('Account successfully created')
+            setIsSigninSuccess(true)
+        }
+    }
+
+    const closeDialog = () => setIsDialogOpen(false)
+
+    const setSuccessDialog = (msg) => {
+        setIsDialogOpen(true)
+        setDialogTitle('Success')
+        setDialogMsg(msg)
+    }
+
+    const setErrorDialog = (msg) => {
+        setIsDialogOpen(true)
+        setDialogTitle('Error')
+        setDialogMsg(msg)
+    }
+
+    return (
+        <Box display={"flex"} flexDirection={"column"} width={"30%"}>
+            <Typography variant={"h3"} marginBottom={"2rem"}>Sign In</Typography>
+            <TextField
+                label="Username"
+                variant="standard"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                sx={{marginBottom: "1rem"}}
+                autoFocus
+            />
+            <TextField
+                label="Password"
+                variant="standard"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{marginBottom: "2rem"}}
+            />
+            <Box display={"flex"} flexDirection={"row"} justifyContent={"flex-end"}>
+                <Button variant={"outlined"} onClick={handleSignin}>Sign in</Button>
+            </Box>
+
+            <Dialog
+                open={isDialogOpen}
+                onClose={closeDialog}
+            >
+                <DialogTitle>{dialogTitle}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>{dialogMsg}</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    {isSigninSuccess
+                        ? <Button component={Link} to="/signin">Log in</Button>
+                        : <Button onClick={closeDialog}>Done</Button>
+                    }
+                </DialogActions>
+            </Dialog>
+        </Box>
+    )
+}
+
+export default SigninPage;
