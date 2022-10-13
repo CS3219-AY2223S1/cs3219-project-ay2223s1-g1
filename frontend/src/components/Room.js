43e3efebcd6a964fccd64bfa84e5857746db1528
itssodium <noorulazlina@gmail.com>
2022-10-13 18:41:04 +0800

    Implement Chat Service

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -7,7 +7,11 @@ import {
     DialogContent,
     DialogContentText,
     TextField,
+    Switch,
+    Grid
+    
 } from "@mui/material";
+
 import { useLocation, Navigate, Link } from "react-router-dom";
 import { io } from "socket.io-client";
 import {useState, useEffect} from "react";
@@ -19,6 +23,7 @@ function Room() {
     const [matchLeaves, setMatchLeaves] = useState(false)
     const [text, setText] = useState("")
     const [messages, setMessage] = useState([])
+    const [checked, setChecked] = useState(true);
 
     const socket = io("http://localhost:8001");
     const socketChat = io("http://localhost:8002");
@@ -31,10 +36,7 @@ function Room() {
                 setMatchLeaves(true)
                 console.log(message)
             })
-        });
-
-        
-    
+        });    
     }, []);
 
     socketChat.on('connect', () => {
@@ -51,7 +53,7 @@ function Room() {
     const sendMessage = async() => {
         const info = [false, text];
         setMessage([...messages, info]);
-        console.log(messages);
+        console.log('switch is', checked);
         socketChat.emit('send', matchName, text);
 
     }
@@ -60,34 +62,57 @@ function Room() {
         socket.emit("leave", name, matchName);
         setIsLeave(true);
     }
+    
 
     return (
-        <Box display={"flex"} flexDirection={"column"} width={"70%"}>
-            <Typography variant={"h5"} marginBottom={"2rem"}>User name: {name}</Typography>
-            <Typography variant={"h5"} marginBottom={"2rem"}>Match name: {matchName}</Typography>
-            <div style={{ width: 200, height: 200, overflowY:"auto"}}>
-                <Box component="div" flex={1} flexDirection="column" display="flex" p={2}>
-                    {messages.map((value) => {
-                        const boolean = value[0];
-                        const mess = value[1];
-                        if (boolean) {
-                            return <Typography variant="body2">{mess}</Typography>    
-                        } else {
-                            return <Typography variant="body2">{mess}</Typography>    
-                        }
-                    })}
+        <Box display={"flex"} flexDirection={"column"} width={"80%"}>
+            <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'row'
+                  }}>
+                <Box width={"50%"}>
+                    <Typography variant={"h5"} marginBottom={"2rem"}>User name: {name}</Typography>
+                    <Typography variant={"h5"} marginBottom={"2rem"}>Match name: {matchName}</Typography>
                 </Box>
-            </div>
-            
-            <TextField
-                label="Message"
-                variant="standard"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                sx={{marginBottom: "1rem"}}
-                autoFocus
-            />
-            <Button onClick={sendMessage}>Send</Button>
+
+                <Box width={"50%"}>
+                    <Switch
+                        checked={checked}
+                        onChange={(e) => setChecked(!checked)}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                    />
+
+                    {checked 
+                        ? <div style={{overflowY:"auto"}}>
+                            <Box component="div" flex={1} flexDirection="column" display="flex" p={1} width={'90%'} height={50}>
+                                {messages.map((value) => {
+                                const boolean = value[0];
+                                const mess = value[1];
+                                if (boolean) {
+                                    return <Grid container>
+                                            <Box typography='body2' bgcolor="#e8eaf6" textAlign='left' width={'auto'} height={'auto'}>{mess}</Box>
+                                        </Grid>        
+                                } else {
+                                    return <Grid container justifyContent="flex-end">
+                                            <Box typography='body2' bgcolor="#ede7f6" textAlign='right' width={'auto'} height={'auto'}>{mess}</Box>
+                                        </Grid>
+                                }
+                            })}
+                            </Box>
+                        </div>
+                        : <Typography variant="body2">Open Chat</Typography>
+                    }
+                    <TextField
+                    label="Message"
+                    variant="standard"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    sx={{marginBottom: "1rem"}}
+                    autoFocus
+                    />
+                    <Button onClick={sendMessage}>Send</Button>
+                </Box>
+            </Box>
 
             <Button onClick={handleLeave}>Leave room</Button>
 
