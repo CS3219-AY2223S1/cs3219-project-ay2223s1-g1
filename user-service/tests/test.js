02653cc520714d363a8a494f9e0fd711e8fcab16
itssodium <noorulazlina@gmail.com>
2022-10-30 11:04:10 +0800

    implement test

---
+++ user-service/tests/test.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http'
+import {app, router} from '../index.js'
+
+chai.use(chaiHttp);
+chai.should();
+
+
+describe('user-controller tes', () => {
+    describe("signIn", () => {
+        it('createUser', (done) => {
+            chai.request(app)
+            .post('/api/user/signin')
+            .send({username:'user1', password:'password1'})
+            .end((err, res) => {
+                done();
+            })
+        })
+    })
+})
+
+
