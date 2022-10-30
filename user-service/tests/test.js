3cf15a8fbdf40587992a083b83f0b5e2f2ffe6c9
itssodium <noorulazlina@gmail.com>
2022-10-30 16:22:56 +0800

    add test cases

--- user-service/tests/test.js
+++ user-service/tests/test.js
@@ -7,12 +7,61 @@ chai.should();
 
 
 describe('user-controller tes', () => {
-    describe("signIn", () => {
-        it('createUser', (done) => {
+    describe("sign up", () => {
+        it('create user', (done) => {
+            chai.request(app)
+            .post('/api/user/signup')
+            .send({username:'user1', password:'password1'})
+            .end((err, res) => {
+                res.should.have.status(201);
+                done();
+            })
+        })
+        it('duplicate user', (done) => {
+            chai.request(app)
+            .post('/api/user/signup')
+            .send({username:'user1', password:'password1'})
+            .end((err, res) => {
+                res.should.have.status(409);
+                done();
+            })
+        })
+        it('user info missing', (done) => {
+            chai.request(app)
+            .post('/api/user/signup')
+            .send()
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+    })
+
+    describe("sign in", () => {
+        it('sign in existing user', (done) => {
             chai.request(app)
             .post('/api/user/signin')
             .send({username:'user1', password:'password1'})
             .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+        it('sign in wrong password', (done) => {
+            chai.request(app)
+            .post('/api/user/signin')
+            .send({username:'user1', password:'password2'})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+        it('user info missing', (done) => {
+            chai.request(app)
+            .post('/api/user/signin')
+            .send()
+            .end((err, res) => {
+                res.should.have.status(400);
                 done();
             })
         })
